bc15bf44e2ba8700813bfb13ad2d9dbfee5dbcb9
avaprin <sergey-gubkin@mail.ru>
2018-09-13 23:42:05 +0300

    lt

--- src/index.js
+++ src/index.js
@@ -3,5 +3,24 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+   let count=0;
+  for(let i = 0; i <preferences.length;)
+  {
+      if(preferences[i]!==-1)
+      {
+          if(preferences[preferences[preferences[i]-1]-1]-1===i)
+          {
+              count++;
+              preferences[preferences[preferences[i]-1]-1]=-1;
+              preferences[preferences[i]-1]=-1;
+              preferences[i]=-1;
+              i++;
+          }
+      }
+      else
+      {
+          i++;
+      }
+  }
+    return count;// your implementation
 };
